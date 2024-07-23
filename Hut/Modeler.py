51f98da087bf47acd523443aa74755f558cd1c09
Kelnit <kelpneedfood500@gmail.com>
2024-07-24 02:10:02 +0700

    Hi !

--- Hut/Modeler.py
+++ Hut/Modeler.py
@@ -21,22 +21,29 @@ class Helper(Model):
 
 from tensorflow.keras.layers import Conv2D, MaxPool2D, Flatten, Dense, Dropout
 
+from tensorflow.keras.layers import Conv2D, MaxPool2D, Flatten, Dense, Dropout
+
 class Hantu(Model):
   def __init__(self):
     super(Hantu, self).__init__()
-    self.imasatu = Conv2D(32, 3, activation="relu")
-    self.imanida = Conv2D(64, 3, activation="relu")
-    self.imatiga = Conv2D(128, 3, activation="relu")
+    self.imasatu = Conv2D(16, 3, activation="relu", padding="same")
+    self.poolone = MaxPool2D((2, 2))
+    self.imanida = Conv2D(32, 3, activation="relu", padding="same")
+    self.pooltwo = MaxPool2D((2, 2))
+    self.imatiga = Conv2D(64, 3, activation="relu", padding="same")
+    self.pooliga = MaxPool2D((2, 2))
+    self.imafour = Conv2D(128, 3, activation="relu", padding="same")
     self.pooling = MaxPool2D((2, 2))
     self.dropout = Dropout(0.3)
     self.flatten = Flatten()
-    self.denseri = Dense(512, activation="relu")
+    self.denseri = Dense(128, activation="relu")
     self.classes = Dense(3, activation="softmax")
 
   def call(self, i):
-    x = self.pooling(self.imasatu(i))
-    x = self.pooling(self.imanida(x))
-    x = self.pooling(self.imatiga(x))
+    x = self.poolone(self.imasatu(i))
+    x = self.pooltwo(self.imanida(x))
+    x = self.pooliga(self.imatiga(x))
+    x = self.pooling(self.imafour(x))
     x = self.dropout(self.flatten(x))
     x = self.denseri(x)
     j = self.classes(x)
